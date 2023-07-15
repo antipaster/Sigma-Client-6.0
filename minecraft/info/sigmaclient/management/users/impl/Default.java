ce8ce3b15a3367fe7a4bd6ff96320762363a2e93
Marcus <121552016+intexpression@users.noreply.github.com>
2023-07-15 16:38:09 +0200

    cum

---
+++ minecraft/info/sigmaclient/management/users/impl/Default.java
@@ -0,0 +1,20 @@
+package info.sigmaclient.management.users.impl;
+
+import info.sigmaclient.management.users.User;
+
+/**
+ * Created by Arithmo on 8/11/2017 at 9:57 PM.
+ */
+public class Default extends User {
+
+
+
+    public void applyModules() {
+        super.applyModules();
+    }
+
+    public void upgradeFeatures() {
+        super.upgradeFeatures();
+    }
+
+}
